f0bfa4610bbff580e8fc985faae745f9a7e28c07
Yaroslav Smirnov <yaroslav.smirnov@inbox.ru>
2016-06-05 15:41:50 +0000

    More commented out sections in server.js and routes.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,13 +7,13 @@ var headerParser = require(path + '/app/controllers/headerParser.server.js');
 
 module.exports = function (app, passport) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	// function isLoggedIn (req, res, next) {
+	// 	if (req.isAuthenticated()) {
+	// 		return next();
+	// 	} else {
+	// 		res.redirect('/login');
+	// 	}
+	// }
 
 	var clickHandler = new ClickHandler();
 	var timeStamp = new TimeStamp();
@@ -41,44 +41,44 @@ module.exports = function (app, passport) {
 
 // The rest is from the ClementineJS FCC boilerplate...
 
-	app.route('/clementine')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
+	// app.route('/clementine')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/index.html');
+	// 	});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
+	// app.route('/login')
+	// 	.get(function (req, res) {
+	// 		res.sendFile(path + '/public/login.html');
+	// 	});
 
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
+	// app.route('/logout')
+	// 	.get(function (req, res) {
+	// 		req.logout();
+	// 		res.redirect('/login');
+	// 	});
 
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
+	// app.route('/profile')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.sendFile(path + '/public/profile.html');
+	// 	});
 
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
+	// app.route('/api/:id')
+	// 	.get(isLoggedIn, function (req, res) {
+	// 		res.json(req.user.github);
+	// 	});
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+	// app.route('/auth/github')
+	// 	.get(passport.authenticate('github'));
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
+	// app.route('/auth/github/callback')
+	// 	.get(passport.authenticate('github', {
+	// 		successRedirect: '/',
+	// 		failureRedirect: '/login'
+	// 	}));
 
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	// app.route('/api/:id/clicks')
+	// 	.get(isLoggedIn, clickHandler.getClicks)
+	// 	.post(isLoggedIn, clickHandler.addClick)
+	// 	.delete(isLoggedIn, clickHandler.resetClicks);
 		
 };
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
-var session = require('express-session');
+// var session = require('express-session');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
+// require('./app/config/passport')(passport);
 
 // mongoose.connect(process.env.MONGO_URI);
 
